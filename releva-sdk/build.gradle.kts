af5b3e235f59c28c4c0239b9ddf794f444faee1e
Yavor Stoychev <stoychev.yavor@gmail.com>
2026-03-09 10:05:14 +0200

    bump version

--- releva-sdk/build.gradle.kts
+++ releva-sdk/build.gradle.kts
@@ -75,7 +75,7 @@ publishing {
         create<MavenPublication>("release") {
             groupId = "ai.releva"
             artifactId = "releva-sdk"
-            version = "1.0.4"
+            version = "1.0.5"
 
             afterEvaluate {
                 from(components["release"])
